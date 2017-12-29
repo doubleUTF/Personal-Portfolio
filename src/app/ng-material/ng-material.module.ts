f52e1726b992b7eb4b9c7cc3343b8ab96514ce96
BigD <dlau373@gmail.com>
2017-12-29 14:36:30 -0800

    Started on project

--- src/app/ng-material/ng-material.module.ts
+++ src/app/ng-material/ng-material.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     CommonModule
+  ],
+  exports:[
+    MatToolbarModule
   ],
   declarations: []
 })
