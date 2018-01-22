02f00a603bbede5830067bb59bfb44944fe72ae6
doubleUTF <dlau373@gmail.com>
2018-01-22 15:58:19 -0800

    Added animation to smooth rendering of background page

--- src/app/stage/stage.component.ts
+++ src/app/stage/stage.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-stage',
   templateUrl: './stage.component.html',
-  styleUrls: ['./stage.component.css']
+  styleUrls: ['./stage.component.css'],
+  animations:[
+    trigger('backgroundState', [
+      state('inactive', style({
+        opacity:0
+      })),
+      state('active', style({
+        opacity:1
+      })),
+      transition('inactive=>active', animate('800ms'))
+    ])
+  ]
 })
+
 export class StageComponent implements OnInit {
 
   constructor() { }
-  fadeIn:Boolean=false;
+
+  state:string='inactive'
+
   ngOnInit() {
-    this.fadeIn=true;
+    setTimeout(()=>{this.state='active'})
+
   }
 
 }
