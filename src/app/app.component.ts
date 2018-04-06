b926fe6c64a270bb6bf77295b68b1a4eb28df3c9
BigD <dlau373@gmail.com>
2018-04-06 12:30:12 -0700

    Fixed flexbox layout by accident :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private route:ActivatedRoute){}
   title = 'app';
+
+  ngOnInit(){
+    console.log(this.route)
+  }
+
 }
