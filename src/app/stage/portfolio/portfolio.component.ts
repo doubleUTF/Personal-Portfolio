e716391557b081ea4527cfd40bb015b0e8d5568f
BigD <dlau373@gmail.com>
2018-04-09 12:29:17 -0700

    Working on portfolio page

--- src/app/stage/portfolio/portfolio.component.ts
+++ src/app/stage/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from './project.model';
 
 @Component({
   selector: 'app-portfolio',
@@ -10,6 +11,44 @@ export class PortfolioComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log(this.projects)
   }
+
+  projects:Project[]=[
+    new Project('Build a Tribute Page', 2016,
+    'Beginner project on FreeCodeCamp',
+    "http://codepen.io/doubleUTF/pen/yaLbGL/image/small.png",
+    "https://codepen.io/doubleUTF/pen/yaLbGL"),
+    new Project("Simon Game", 2017,
+    "Classic 80's Game",
+    "../../../assets/img/simon.JPG",
+    "https://codepen.io/doubleUTF/pen/qPyJaX"
+    ),
+    new Project('Tic Tac Toe', 2017,
+    'Original Tic Tac Toe game made using minimax algorithm',
+    "../../../assets/img/ttt.jpg",
+    "https://codepen.io/doubleUTF/pen/MvqKGO"
+      ),
+    new Project('Wiki Viewer', 2017,
+      'Custom view of random or searched wiki articles.',
+    "../../../assets/img/wiki_viewer.jpg",
+    "https://codepen.io/doubleUTF/pen/ZJzaeo"),
+    new Project('Calculator', 2017,
+    "Javascript implementation of a basic calculator",
+    "../../../assets/img/calc.jpg",
+    "https://codepen.io/doubleUTF/pen/XajMGM"
+    ),
+    new Project('Pomodoro Clock', 2017,
+    'Timer app with progress animation',
+    "../../../assets/img/pomodoro_clock.jpg",
+    "https://codepen.io/doubleUTF/pen/KvWYrx"
+    ),
+    new Project('Alex Lau Acupuncture', 2017,
+    'Business page for prominent acupuncture clinic',
+    "../../../assets/img/alex_lau_acupuncture.jpg",
+    "http://aculau.com"
+    )
+
+  ];
 
 }
---
+++ src/app/stage/portfolio/project.model.ts
@@ -0,0 +1,9 @@
+export class Project{
+  constructor(
+    public name:string,
+    public year:number,
+    public description:string,
+    public imagePath:string,
+    public link?:string,
+  ){}
+}
