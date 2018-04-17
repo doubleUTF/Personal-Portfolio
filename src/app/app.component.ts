2af985b23d86f428800f22bd7eb2b252c97349bb
BigD <dlau373@gmail.com>
2018-04-17 11:00:37 -0700

    Sped up the animation fade in time from 800ms to 700ms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {fadeIn} from './stage/stage.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations:[fadeIn]
 })
 
 export class AppComponent implements OnInit {
