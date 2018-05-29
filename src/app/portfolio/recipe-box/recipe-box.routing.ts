65a29df7cda2c8e7cc64720ef6742b38206930d3
doubleUTF <dlau373@gmail.com>
2018-05-29 15:31:09 -0700

    Updated the recipe-box routing to include data for nav buttons

--- src/app/portfolio/recipe-box/recipe-box.routing.ts
+++ src/app/portfolio/recipe-box/recipe-box.routing.ts
@@ -3,11 +3,13 @@ import {Routes,RouterModule} from '@angular/router';
 import {RecipeBoxComponent} from './recipe-box.component';
 import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
 import {RecipeThumbnailsComponent} from './recipe-thumbnails/recipe-thumbnails.component';
+import {RecipeEditComponent} from './recipe-edit/recipe-edit.component';
 
 const recipeRoutes:Routes=[
   {path:'',component:RecipeBoxComponent,children:[
-    {path:'',component:RecipeThumbnailsComponent},
-    {path:':id', component:RecipeDetailComponent}
+    {path:'',component:RecipeThumbnailsComponent, data:{navButtons:['add']}},
+    {path:':id', component:RecipeDetailComponent, data:{navButtons:['edit']}},
+    {path:':id/edit', component:RecipeEditComponent, data:{navButtons:['cancel','save']}}
   ]},
 ]
 
--- src/app/portfolio/recipe-box/recipe-edit/recipe-edit.component.ts
+++ src/app/portfolio/recipe-box/recipe-edit/recipe-edit.component.ts
@@ -12,4 +12,7 @@ export class RecipeEditComponent implements OnInit {
   ngOnInit() {
   }
 
+  newRecipe:boolean;
+
+  
 }
