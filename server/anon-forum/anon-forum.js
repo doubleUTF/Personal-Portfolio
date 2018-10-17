554702a7301a30a14277909fbb1819fd34ca12fa
BigD <dlau373@gmail.com>
2018-10-17 00:04:43 -0700

    Fixed content cards not preserving line breaks. Added few more seed data to technical support. Changed primary color to aqua. Disabled testing code

--- server/anon-forum/anon-forum.js
+++ server/anon-forum/anon-forum.js
@@ -3,6 +3,7 @@
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
+const {populateBoard,populateThread, populateReply,threadOneId, clearTest}=require('./tests/seed');
 
 const anonForum=(app)=>{
 
@@ -13,17 +14,22 @@ const anonForum=(app)=>{
   apiRoutes(app);
 
   // Start our server and tests!
-  console.log('Running Tests...');
-  setTimeout(function () {
-    try {
-      runner.run();
-    } catch(e) {
-      var error = e;
-        console.log('Tests are not valid:');
-        console.log(error);
-    }
-  }, 3000);
+  // console.log('Running Tests...');
+  // setTimeout(function () {
+  //   try {
+  //     runner.run();
+  //   } catch(e) {
+  //     var error = e;
+  //       console.log('Tests are not valid:');
+  //       console.log(error);
+  //   }
+  // }, 3000);
 
+  // Seed some dummy data, delete after first run
+  clearTest()
+  populateBoard()
+  populateThread()
+  populateReply()
 }
 
 module.exports = anonForum; //for testing
--- server/anon-forum/tests/seed.js
+++ server/anon-forum/tests/seed.js
@@ -5,7 +5,8 @@ const Thread= require('../models/thread');
 const loremIpsum = require('lorem-ipsum');
 const ObjectId=require('mongodb').ObjectId;
 
-const boardId=new ObjectId();
+const boardId1=new ObjectId();
+const boardId2=new ObjectId();
 const threadOneId=new ObjectId();
 const threadTwoId=new ObjectId();
 const threadThreeId=new ObjectId();
@@ -17,22 +18,25 @@ const threadEightId=new ObjectId();
 const threadNineId=new ObjectId();
 const threadTenId=new ObjectId();
 const threadElevenId=new ObjectId();
+const threadTwelveId=new ObjectId();
 
 const reply1Id=new ObjectId();
 const reply2Id=new ObjectId();
 const reply3Id=new ObjectId();
 const reply4Id=new ObjectId();
+const reply5Id=new ObjectId();
 
 const salt=bcrypt.genSaltSync(10);
-const delete_password=bcrypt.hashSync('a',salt);
+const password='a'
+const delete_password=bcrypt.hashSync(password,salt);
 const boards=[
-  {title:'Technical Support', delete_password,
+  {title:'Technical Support', delete_password, _id:boardId2,
   description:'Post technical-related issues.'},
   {title:'General Discussion',delete_password,
   description:'Discuss any topic that comes to mind.'},
   {title:'Suggestions',delete_password,
   description:'Give your thoughts about this forum.'},
-  {title:'Lorem Ipsum',_id:boardId,delete_password,
+  {title:'Lorem Ipsum',_id:boardId1,delete_password,
   description:'Many lorem ipsums.'},
   {title:'Job Openings', delete_password,
   description:'Available positions for employment.'},
@@ -44,100 +48,116 @@ const boards=[
   description:'Memes and dreams.'}
 ];
 
+const showDeletepassword=`. Delete password is ${password}`;
 const threads=[
   {
-    text:loremIpsum({count:Math.random()*10,units:'sentences'}),
+    text:loremIpsum({count:Math.random()*10,units:'sentences'})+showDeletepassword,
     delete_password,
     created_on:new Date().setFullYear(2000),
     bumped_on:new Date().setFullYear(2018),
     reported:false,
     _id:threadOneId,
-    board:boardId,
+    board:boardId1,
     replies:[reply1Id,reply2Id,reply3Id,reply4Id]
 },{
-  text:loremIpsum({count:Math.random()*10,units:'sentences'}),
+  text:loremIpsum({count:Math.random()*10,units:'sentences'})+showDeletepassword,
   delete_password,
   created_on:new Date().setFullYear(2001),
   bumped_on:new Date().setFullYear(2001),
   reported:true,
   _id:threadTwoId,
-  board:boardId,
+  board:boardId1,
 },{
-  text:loremIpsum({count:Math.random()*10,units:'sentences'}),
+  text:loremIpsum({count:Math.random()*10,units:'sentences'})+showDeletepassword,
   delete_password,
   created_on:new Date().setFullYear(2002),
   bumped_on:new Date().setFullYear(2002),
   reported:false,
   _id:threadThreeId,
-  board:boardId,
+  board:boardId1,
 },{
-  text:loremIpsum({count:Math.random()*10,units:'sentences'}),
+  text:loremIpsum({count:Math.random()*10,units:'sentences'})+showDeletepassword,
   delete_password,
   created_on:new Date().setFullYear(2003),
   bumped_on:new Date().setFullYear(2003),
   reported:false,
   _id:threadFourId,
-  board:boardId,
+  board:boardId1,
 },{
-  text:loremIpsum({count:Math.random()*10,units:'sentences'}),
+  text:loremIpsum({count:Math.random()*10,units:'sentences'})+showDeletepassword,
   delete_password,
   created_on:new Date().setFullYear(2004),
   bumped_on:new Date().setFullYear(2004),
   reported:true,
   _id:threadFiveId,
-  board:boardId,
+  board:boardId1,
 },{
-  text:loremIpsum({count:Math.random()*10,units:'sentences'}),
+  text:loremIpsum({count:Math.random()*10,units:'sentences'})+showDeletepassword,
   delete_password,
   created_on:new Date().setFullYear(2005),
   bumped_on:new Date().setFullYear(2005),
   reported:true,
   _id:threadSixId,
-  board:boardId,
+  board:boardId1,
 },{
-  text:loremIpsum({count:Math.random()*10,units:'sentences'}),
+  text:loremIpsum({count:Math.random()*10,units:'sentences'})+showDeletepassword,
   delete_password,
   created_on:new Date().setFullYear(2006),
   bumped_on:new Date().setFullYear(2006),
   reported:false,
   _id:threadSevenId,
-  board:boardId,
+  board:boardId1,
 },{
-  text:loremIpsum({count:Math.random()*10,units:'sentences'}),
+  text:loremIpsum({count:Math.random()*10,units:'sentences'})+showDeletepassword,
   delete_password,
   created_on:new Date().setFullYear(2007),
   bumped_on:new Date().setFullYear(2007),
   reported:true,
   _id:threadEightId,
-  board:boardId,
+  board:boardId1,
 },{
-  text:loremIpsum({count:Math.random()*10,units:'sentences'}),
+  text:loremIpsum({count:Math.random()*10,units:'sentences'})+showDeletepassword,
   delete_password,
   created_on:new Date().setFullYear(2008),
   bumped_on:new Date().setFullYear(2008),
   reported:true,
   _id:threadNineId,
-  board:boardId,
+  board:boardId1,
 },{
-  text:loremIpsum({count:Math.random()*10,units:'sentences'}),
+  text:loremIpsum({count:Math.random()*10,units:'sentences'})+showDeletepassword,
   delete_password,
   created_on:new Date().setFullYear(2009),
   bumped_on:new Date().setFullYear(2009),
   reported:false,
   _id:threadTenId,
-  board:boardId,
+  board:boardId1,
 },{
-  text:loremIpsum({count:Math.random()*10,units:'sentences'}),
+  text:loremIpsum({count:Math.random()*10,units:'sentences'})+showDeletepassword,
   delete_password,
   created_on:new Date().setFullYear(2010),
   bumped_on:new Date().setFullYear(2010),
   reported:false,
   _id:threadElevenId,
-  board:boardId,
+  board:boardId1,
 },
+{text:`Hi everyone,
+I have almost finished my documentation page but I am having an issue with the scroll to # section. When you click on a link it stops below the title. Does anyone know how to fix it? I want to be able to see the title of the section when I click on a link.
+Also, I would love any other feedback =)
+Thank you so much`,
+delete_password,
+reported:false,
+_id:threadTwelveId,
+board:boardId2,
+replies:[reply5Id]
+}
 ]
 
 const replies=[
+  {text:`Hi, the issue with the links not traveling to the intended header position is because the fixed header covers the destination of the anchor links. Add this to your CSS for a workaround (source:https://stackoverflow.com/questions/4086107/fixed-page-header-overlaps-in-page-anchors 1):
+section:before{ content:''; display:block; height:60px; margin-top:-60px; }`,
+delete_password,
+thread:threadTwelveId,
+_id:reply5Id},
   {
     text:loremIpsum({count:Math.random()*10,units:'sentences'}),
     delete_password,
