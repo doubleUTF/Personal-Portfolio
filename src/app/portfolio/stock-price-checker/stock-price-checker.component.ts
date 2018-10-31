d5e100e1522cd65a811414cad76b9b4105962e12
BigD <dlau373@gmail.com>
2018-10-31 12:05:55 -0700

    Still playing around with angular animations

--- src/app/portfolio/stock-price-checker/stock-price-checker.component.ts
+++ src/app/portfolio/stock-price-checker/stock-price-checker.component.ts
@@ -1,28 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormControl, Validators } from '@angular/forms';
-import {trigger, state, style, animate,transition} from '@angular/animations';
+import {trigger, style, animate,transition} from '@angular/animations';
+import {StockPriceCheckerService} from './stock-price-checker.service';
 
 @Component({
   selector: 'app-stock-price-checker',
   templateUrl: './stock-price-checker.component.html',
   styleUrls: ['./stock-price-checker.component.css'],
+  providers:[StockPriceCheckerService],
   animations:[
-    state('open',style({
-      transform:'translateX(0)',
-      opacity:1
-    })),
-    state('closed',style({
-      transform:'translateX(-300px)',
-      opacity:0
-    })),
-    transition('open=>closed',[
-      animate('1s')
+    trigger('enterLeave',[
+      transition(':enter',[
+        style({
+          transform:'translateX(100%)',
+          opacity:0,
+        }),
+        animate('.8s ease-out', style({
+          transform:'translateX(0)',
+          opacity:1,
+        }))
+      ]),
+      transition(':leave',[
+        animate('.8s ease-out', style({
+          opacity:0,
+          transform:'translateX(-100%)'
+        }))
+      ])
+    ]),
+    trigger('newEnterLeave',[
+      transition(':enter',[
+        style({
+          transform:'translateX(100%)',
+          opacity:0
+        }),
+        animate('.8s ease-out', style({
+          transform:'translateX(0)',
+          opacity:1
+        }))
+      ]),
+      transition(':leave',[
+        animate('.8s ease-out', style({
+          transform:'translateX(-100%)',
+          opacity:0
+        }))
+      ])
     ])
   ]
 })
+
 export class StockPriceCheckerComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private spcs:StockPriceCheckerService
+  ) { }
 
   stockForm = new FormGroup({
     stock1: new FormControl('',Validators.required),
@@ -30,6 +60,7 @@ export class StockPriceCheckerComponent implements OnInit {
     like: new FormControl()
   });
 
+  stocks;
   ngOnInit() {
   }
   newStock:boolean=true; // Flag to control display of new stock form
@@ -39,6 +70,16 @@ export class StockPriceCheckerComponent implements OnInit {
   }
 
   getStocks(){
-    this.newStock=false;
+    this.newStock=!this.newStock;
+    this.spcs.getDummyStocks(this.stockForm.value.stock1,this.stockForm.value.stock2).then(data=>{
+      this.stocks=data;
+      console.log(data)
+    })
+  }
+
+  newQuery(){
+    this.newStock=true;
+    this.stocks=null;
+    this.stockForm.reset()
   }
 }
--- src/app/portfolio/stock-price-checker/stock-price-checker.service.ts
+++ src/app/portfolio/stock-price-checker/stock-price-checker.service.ts
@@ -6,4 +6,12 @@ import { Injectable } from '@angular/core';
 export class StockPriceCheckerService {
 
   constructor() { }
-}
+
+  getDummyStocks(stock1:string, stock2?:string):Promise<Object>{
+    return new Promise((res,rej)=>{
+      setTimeout(()=>{
+        res({stock1:{name:stock1, price:1232.12, rel_likes:5}, stock2:{name:stock2,price:321.06,rel_likes:-5}})
+      },1000)
+    })
+    }
+  }
