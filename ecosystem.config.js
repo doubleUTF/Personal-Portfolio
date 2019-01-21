e273b75dfdd64549e1829b699631acc88f5781ac
David <dlau373@gmail.com>
2019-01-21 10:35:07 -0800

    Edited max_memory_restart to 128mb. Updated PM2. PM2 is constantly restarting and causing memory leaks. Hopefully this will address the problem. Otherwise just remove PM2 from process.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
     instances: 1,
     autorestart: true,
     watch: true,
-    max_memory_restart: '1G',
+    max_memory_restart: '128M',
     env: {
       NODE_ENV: 'development'
     },
